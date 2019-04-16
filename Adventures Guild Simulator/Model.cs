df988f5a29f726a346221a0d3be1295d64dc1b7e
Mathias <Mathias@LAPTOP-9FSN3NE0>
2019-04-16 13:19:55 +0200

    derp

---
+++ Adventures Guild Simulator/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventures_Guild_Simulator
+{
+    class Model
+    {
+        const string CONNECTIONSTRING = @"Data Source=AGS.db;version=3;New=true;Compress=true";
+        protected SQLiteConnection connection = new SQLiteConnection(CONNECTIONSTRING);
+
+        public Model()
+        {
+            connection.Open();
+        }
+    }
+}
